2629a151f3daac95172a25cb422fa7cbf1b72dbe
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-03-06 12:18:42 -0300

    Utlizando props e emissão de eventos para acessar atributos definidos no componente pai.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h3>Hey</h3>
+  <greeting :age="age"></greeting>
+  <user
+    :age="age"
+    @age-change-increase="updateAge"
+    @age-change-decrease="age--"
+    :ageChangeFn="updateAgeCB"
+  ></user>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Greeting from "./components/GreetingComponent.vue";
+import User from "./components/UserComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Greeting,
+    User,
+  },
+  data() {
+    return {
+      age: 20,
+    };
+  },
+  methods: {
+    updateAge(num) {
+      if (this.age + num > 130) {
+        this.age = 130;
+        return;
+      }
+      this.age += num;
+    },
+    updateAgeCB(num) {
+      this.age += num;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/UserComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <button type="button" @click="onClickIncreaseAge">Increase age</button>
+  <button type="button" @click="ageChangeFn(3)">Increase age</button>
+  <button type="button" @click="onClickDecreaseAge">Decrease age</button>
+  <p>The user is {{ age }} years old</p>
+  <p>{{ ageDoubled }}</p>
+</template>
+
+<script>
+export default {
+  name: "UserComponent",
+  props: {
+    age: {
+      type: Number,
+      //required: true,
+      //   default: 20,
+      validator(value) {
+        return value < 130;
+      },
+    },
+    ageChangeFn: Function,
+  },
+  emits: ["age-change-increase", "age-change-decrease"],
+  computed: {
+    ageDoubled() {
+      return this.age * 2;
+    },
+  },
+  methods: {
+    onClickIncreaseAge() {
+      this.$emit("age-change-increase", 3);
+    },
+    onClickDecreaseAge() {
+      this.$emit("age-change-decrease");
+    },
+  },
+};
+</script>
